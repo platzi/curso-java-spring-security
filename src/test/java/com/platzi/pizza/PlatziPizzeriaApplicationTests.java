75c0f635b970d2e040b79aab1b849bc75e56ad6a
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2023-03-06 09:46:52 -0500

    Initial commit

---
+++ src/test/java/com/platzi/pizza/PlatziPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
