6f5be631ce5554cfe4544db1b9c47d6244e5bd56
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2023-03-13 11:21:46 -0500

    Configuración inicial

---
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.platzi.pizza.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
