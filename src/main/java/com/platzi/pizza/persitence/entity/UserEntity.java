d23d9332c79f1eafc4d08d4460fd891ad5eeec16
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2023-03-14 10:21:33 -0500

    Se crea entity UserEntity.

---
+++ src/main/java/com/platzi/pizza/persitence/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.platzi.pizza.persitence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean locked;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private Boolean disabled;
+}
