ed5a5b5db7923bc9cd51cf3c8716a86192d80e61
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2023-03-14 12:13:26 -0500

    Agregar JWT.

---
+++ src/main/java/com/platzi/pizza/web/config/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.platzi.pizza.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+    private static String SECRET_KEY = "pl4tz1_p1zz4";
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("platzi-pizza")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
