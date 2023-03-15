304c3cf31c2c20b026b320396f192e46f374e81b
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2023-03-15 10:48:00 -0500

    Auditar usuario.

---
+++ src/main/java/com/platzi/pizza/persitence/audit/AuditUsername.java
@@ -0,0 +1,24 @@
+package com.platzi.pizza.persitence.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuditUsername implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        String username = authentication.getPrincipal().toString();
+
+        return Optional.of(username);
+    }
+}
--- src/main/java/com/platzi/pizza/persitence/audit/AuditableEntity.java
+++ src/main/java/com/platzi/pizza/persitence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.platzi.pizza.persitence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -16,4 +18,12 @@ public class AuditableEntity {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
 }
