6703001bf317496deb3887fa51888e6474c20c1f
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2023-03-15 09:05:56 -0500

    Validar JWT.

--- src/main/java/com/platzi/pizza/web/config/JwtUtil.java
+++ src/main/java/com/platzi/pizza/web/config/JwtUtil.java
@@ -2,6 +2,7 @@ package com.platzi.pizza.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -19,5 +20,23 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String jwt) {
+        try {
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String jwt) {
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
